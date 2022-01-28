a7e436e8cffd2e39c179d0df99c3488d4e95db06
AhmadDalao <shadow8evil@gmail.com>
2022-01-28 14:18:51 +0300

    added main menu and added play again and quit buttons

--- Assets/scripts/gameManager.cs
+++ Assets/scripts/gameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class gameManager : MonoBehaviour
 {
@@ -15,5 +16,29 @@ public class gameManager : MonoBehaviour
     {
     }
 
+    // load the current scene on button click 
+    public void playAgainButton()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    // Quit the game on button click.
+    public void quitGameButton()
+    {
+        Application.Quit();
+    }
+
+
+    // play game from main menu 
+    public void playGameButton()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
 
 }
